fa5d22e02e4c7b0c48d1db94dff3dbc7d333ea63
protonn <support@argentum.io>
2018-01-10 19:47:48 -0500

    Increase the number of round we do before generating a new block template when CPU mining

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -98,7 +98,7 @@ UniValue getnetworkhashps(const JSONRPCRequest& request)
 
 UniValue generateBlocks(boost::shared_ptr<CReserveScript> coinbaseScript, int nGenerate, uint64_t nMaxTries, bool keepScript)
 {
-    static const int nInnerLoopCount = 0x10000;
+    static const int nInnerLoopCount = 0x100000;
     int nHeightStart = 0;
     int nHeightEnd = 0;
     int nHeight = 0;
